3004685b9fae9ce8b6717ef8ba29129ca8b17205
Ben Thompson <t.ben.thompson@gmail.com>
2022-07-05 17:46:00 -0400

    Flattening and unflattening.

--- inla_survival/inla.py
+++ inla_survival/inla.py
@@ -7,7 +7,6 @@ import numpyro
 import numpyro.distributions as dist
 import numpyro.handlers as handlers
 
-# TODO: Tests for the inversion routines.
 # TODO: Test against MCMC.
 # TODO: test multiple n_arms
 # TODO: test automatic gradients and explicit gradients
@@ -31,35 +30,128 @@ def merge(*pytrees):
     return jax.tree_map(_merge, *pytrees, is_leaf=lambda x: x is None)
 
 
-def build_raw_log_likelihood(model, example_p):
-    jax.tree_util.tree
-    ravel_p, unravel_fnc = jax.flatten_util.ravel_pytree(example_p)
-    ravel_fnc = lambda p: jax.flatten_util.ravel_pytree(p)[0]
-    which_nan = jnp.isnan(ravel_p)
-    mult = jax.tree_util.tree_map(lambda x: jnp.any(jnp.isnan(x)).astype(int), example_p)
-
+def build_log_likelihood(model):
     def log_likelihood(p, p_pinned, data):
-        param_array = jnp.where(~jnp.array([True, False, False, False, False]), p, p_pinned)
+        # Inputs are pytrees
+        p_final = merge(p, p_pinned)
         seeded_model = handlers.seed(model, jax.random.PRNGKey(10))
-        subs_model = handlers.substitute(seeded_model, unravel_fnc(param_array))
+        subs_model = handlers.substitute(seeded_model, p_final)
         trace = handlers.trace(subs_model).get_trace(data)
         return sum(
             [jnp.sum(site["fn"].log_prob(site["value"])) for k, site in trace.items()]
         )
 
-    return log_likelihood, ravel_fnc, unravel_fnc
+    return log_likelihood
 
+def build_ravel_fncs(param_ex):
+    # The ordering of entries in the concatenated grad/hess here will depend on
+    # the order of entries in the param_ex dictionary. Since Python 3.6,
+    # dictionaries are insertion ordered so this depends on user choice of
+    # parameter order. But, because we fix it once ahead of time, later
+    # inconsistency will be just fine!
+    key_order = param_ex.keys()
+    def ravel_f(p, axis=-1):
+        return jnp.concatenate(
+            [
+                p[k][~jnp.isnan(param_ex[k])]
+                for k in key_order
+                if param_ex[k] is not None
+            ],
+            axis=axis,
+        )
 
-def build_grad_hess(ll_fnc):
+    def unravel_f(x):
+        out = dict()
+        i = 0
+        for k in key_order:
+            if param_ex[k] is None:
+                out[k] = None
+                continue
+            end = i + param_ex[k].shape[0]
+            out_arr = jnp.full(param_ex[k].shape[0], jnp.nan)
+            out[k] = out_arr.at[jnp.where(~jnp.isnan(param_ex[k]))[0]].set(x[i:end])
+            i = end
+        return out
+
+    return ravel_f, unravel_f, key_order
+
+def build_grad_hess(log_joint, param_ex, pin_ex):
+    no_pinning = jax.tree_util.tree_all(
+        jax.tree_util.tree_map(lambda x: x is None, pin_ex)
+    )
+    if not no_pinning:
+        isnan_pytree = [jnp.isnan(pin_ex[k]) for k in pin_ex.keys()]
+        not_pinned = jnp.concatenate([v for v in isnan_pytree if not jnp.all(~v)])
+
     def grad_hess(p, p_pinned, data):
-        grad = jax.grad(ll_fnc)(p, p_pinned, data)
-        hess = jax.hessian(ll_fnc)(p, p_pinned, data)
-        return grad, hess
-    return grad_hess
+        # The inputs to grad_hess are pytrees but the output grad/hess are
+        # flattened.
+        grad = jax.grad(log_joint)(p, p_pinned, data)
+        hess = jax.hessian(log_joint)(p, p_pinned, data)
 
-    # return jax.jit(
-    #     jax.vmap(jax.vmap(grad_hess, in_axes=(0, 0, None)), in_axes=(0, None, 0))
-    # )
+        full_grad = jnp.concatenate(
+            [grad[k] for k in p.keys() if grad[k] is not None], axis=-1
+        )
+        full_hess = jnp.concatenate(
+            [
+                jnp.concatenate(
+                    [hess[k1][k2] for k2 in p.keys() if hess[k1][k2] is not None],
+                    axis=-1,
+                )
+                for k1 in p.keys()
+                if hess[k1] is not None
+            ],
+            axis=-2,
+        )
+        if no_pinning:
+            return full_grad, full_hess
+        else:
+            return full_grad[not_pinned], full_hess[not_pinned][:, not_pinned]
+
+    return jax.jit(
+        jax.vmap(jax.vmap(grad_hess, in_axes=(0, 0, None)), in_axes=(0, None, 0))
+    )
+
+
+# TODO: how do I unflatten from the x array into the p dict
+def build_step_hess(log_joint, pin_ex):
+    grad_hess = build_grad_hess(log_joint, pin_ex)
+
+    def step_hess(x, p_pinned, data):
+        # Inputs and outputs are arrays, need to convert to pytrees internally.
+        grad, hess = grad_hess(p, p_pinned, data)
+        d = grad.shape[-1]
+        solver = jax.vmap(jax.vmap(smalljax.gen(f"solve{d}")))
+        return -solver(hess, grad)
+
+    return step_hess
+
+
+def build_optimizer(
+    log_joint=None, pin_ex=None, step_hess=None, max_iter=30, tol=1e-3
+):
+    if step_hess is None:
+        if log_joint is None or pin_ex is None:
+            raise ValueError(
+                "Either step_hess must be specified or both log_joint and "
+                "pin_ex must be specified."
+            )
+        step_hess = build_grad_hess(log_joint, pin_ex)
+
+    def optimize_loop(x0, p_pinned, data):
+        def step(args):
+            x, hess_info, iters, go = args
+            step, hess_info = step_hess(x, p_pinned, data)
+            go = jnp.any(jnp.sum(step**2) > tol**2) & (iters < max_iter)
+            return x + step, hess_info, iters + 1, go
+
+        step0, hess_info0 = step_hess(x0, p_pinned, data)
+        x, hess_info, iters, go = jax.lax.while_loop(
+            lambda args: args[3], step, (x0 + step0, hess_info0, 1, True)
+        )
+        return x, hess_info, iters
+
+    return jax.jit(optimize_loop)
 
 
 class INLA:
--- inla_survival/test_numpyro_inla.py
+++ inla_survival/test_numpyro_inla.py
@@ -10,6 +10,7 @@ import inla
 import util
 
 from jax.config import config
+
 config.update("jax_enable_x64", True)
 
 mu_0 = -1.34
@@ -32,13 +33,16 @@ def berry_model(d):
             dist.BinomialLogits(theta + logit_p1, total_count=data[..., 1]),
             obs=data[..., 0],
         )
+
     return model
 
 
 def test_log_likelihood():
     params = dict(sig2=10.0, theta=np.array([0, 0, 0]))
     data = np.array([[6.0, 35], [5, 35], [4, 35]])
-    ll = inla.build_log_likelihood(berry_model(3))(params, data)
+    ll = inla.build_log_likelihood(berry_model(3))(
+        params, dict(sig2=None, theta=None), data
+    )
 
     invgamma_term = scipy.stats.invgamma.logpdf(
         params["sig2"], sig2_alpha, scale=sig2_beta
@@ -53,12 +57,64 @@ def test_log_likelihood():
     np.testing.assert_allclose(
         ll, invgamma_term + normal_term + sum(binomial_term), rtol=1e-6
     )
-    
+
+
 def test_merge():
-    a, b = dict(sig2 = 10.0, theta = jnp.array([3, np.nan])), dict(sig2 = None, theta=jnp.array([np.nan, 2]))
+    a, b = dict(sig2=10.0, theta=jnp.array([3, np.nan])), dict(
+        sig2=None, theta=jnp.array([np.nan, 2])
+    )
     out = inla.merge(a, b)
-    assert(out['sig2'] == 10.0)
-    np.testing.assert_allclose(out['theta'], [3, 2])
+    assert out["sig2"] == 10.0
+    np.testing.assert_allclose(out["theta"], [3, 2])
+
+def test_ravel_fncs():
+    tt = np.arange(4, dtype=np.float64)
+    tt[1] = np.nan
+    ex = dict(sig2=None, theta=tt)
+    ravel_f, unravel_f = inla.build_ravel_fncs(ex)
+    r = ravel_f(ex)
+    np.testing.assert_allclose(r, [0,2,3])
+    ur = unravel_f(r)
+    for k in ex:
+        if ex[k] is None:
+            assert(ur[k] is None)
+            continue
+        np.testing.assert_allclose(ur[k], ex[k])
+
+def test_grad_hess():
+    data = np.array([[7, 35], [6.0, 35], [5, 35], [4, 35]])
+    params = dict(
+        sig2=10.0,
+        theta=np.array([0.0, 0.0, 0, 0]),
+    )
+    ll_fnc = inla.build_log_likelihood(berry_model(4))
+    grad_hess_vmap = inla.build_grad_hess(ll_fnc, dict(sig2=None, theta=None))
+    grad, hess = grad_hess_vmap(
+        dict(sig2=np.full((1, 1, 1), 10.0), theta=np.zeros((1, 1, 4))),
+        dict(sig2=None, theta=None),
+        data[None],
+    )
+    full_grad = np.array([-0.25124812, -3.5032682, -4.5032682, -5.5032682, -6.5032682])
+    hess01 = np.array([
+        [2.5007863e-02, 7.9709353e-06, 7.9717356e-06, 7.9713354e-06, 7.9718011e-06],
+        [7.9710153e-06, -7.4256096e00, 2.4390254e-02, 2.4390249e-02, 2.4390247e-02],
+    ])
+    np.testing.assert_allclose(grad[0, 0], full_grad, rtol=1e-4)
+    np.testing.assert_allclose(hess[0, 0, :2], hess01, rtol=1e-4)
+
+    theta_fixed = jnp.array([np.nan, 0.0, np.nan, np.nan])
+    grad_hess_vmap = inla.build_grad_hess(
+        ll_fnc,
+        dict(sig2=jnp.array([10.0]), theta=theta_fixed),
+    )
+    grad, hess = grad_hess_vmap(
+        dict(sig2=None, theta=np.zeros((2, 1, 4))),
+        dict(sig2=np.array([[10.0]]), theta=theta_fixed[None]),
+        np.tile(data[None], (2, 1, 1)),
+    )
+    np.testing.assert_allclose(grad[0, 0], full_grad[[1,3,4]], rtol=1e-4)
+    np.testing.assert_allclose(hess[0, 0, 0], hess01[1,[1,3,4]], rtol=1e-4)
+
 
 def test_optimize_posterior():
     N = 10
@@ -67,48 +123,34 @@ def test_optimize_posterior():
     data = np.stack((y_i, n_i), axis=-1).astype(np.float64)
     ll_fnc = inla.build_raw_log_likelihood(berry_model(4))
 
-    # def conditional(theta, sig2, data):
-    #     params = dict(theta=theta, sig2=sig2)
-    #     return ll_fnc(params, dict(sig2=None, theta=None), data)
-
-
-    # def grad_hess(theta, sig2, data):
-    #     grad = jax.grad(conditional)(theta, sig2, data)
-    #     hess = jax.hessian(conditional)(theta, sig2, data)
-    #     return grad, hess
-
-    # grad_hess_vmap = jax.jit(
-    #     jax.vmap(jax.vmap(grad_hess, in_axes=(0, 0, None)), in_axes=(0, None, 0))
-    # )
-    # conditional_vmap = jax.jit(
-    #     jax.vmap(jax.vmap(conditional, in_axes=(0, 0, None)), in_axes=(0, None, 0))
-    # )
     infer = inla.INLA(ll_fnc, 4)
     sig2_rule = util.log_gauss_rule(15, 1e-6, 1e3)
+    inla.build_optimizer(ll_fnc)
     theta_max, hess, iters = infer.optimize_loop(data, sig2_rule.pts, 1e-3)
-    post = infer.posterior(theta_max, hess, sig2_rule.pts, sig2_rule.wts, data)
     np.testing.assert_allclose(
         theta_max[0, 12],
         np.array([-6.04682818, -2.09586893, -0.21474981, -0.07019088]),
         atol=1e-3,
     )
-    correct = np.array(
-        [
-            1.25954474e02,
-            4.52520893e02,
-            8.66625278e02,
-            5.08333300e02,
-            1.30365045e02,
-            2.20403048e01,
-            3.15183578e00,
-            5.50967224e-01,
-            2.68365061e-01,
-            1.23585852e-01,
-            1.13330444e-02,
-            5.94800210e-04,
-            4.01075571e-05,
-            4.92782335e-06,
-            1.41605356e-06,
-        ]
-    )
-    np.testing.assert_allclose(post[0], correct, rtol=1e-3)
+
+    # post = infer.posterior(theta_max, hess, sig2_rule.pts, sig2_rule.wts, data)
+    # correct = np.array(
+    #     [
+    #         1.25954474e02,
+    #         4.52520893e02,
+    #         8.66625278e02,
+    #         5.08333300e02,
+    #         1.30365045e02,
+    #         2.20403048e01,
+    #         3.15183578e00,
+    #         5.50967224e-01,
+    #         2.68365061e-01,
+    #         1.23585852e-01,
+    #         1.13330444e-02,
+    #         5.94800210e-04,
+    #         4.01075571e-05,
+    #         4.92782335e-06,
+    #         1.41605356e-06,
+    #     ]
+    # )
+    # np.testing.assert_allclose(post[0], correct, rtol=1e-3)
