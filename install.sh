b103e203eec8c9d8e63b6e0fa933375fd7a6acbc
Ben Thompson <t.ben.thompson@gmail.com>
2022-07-27 21:09:51 +0000

    Splitting out development tools into environment-dev.yml

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# TODO: could also set up conda environment here? or is that too magic?
 # List the internal packages that should be available here.
 for PKG in ./inlaw ./research/berry ./imprint/python
 do
-    [ -d "$PKG" ] && echo "Directory $PKG exists" && pip install --no-deps -e "$PKG"
+    [ -d "$PKG" ] && echo "\nDirectory $PKG exists" && pip install --no-deps -e "$PKG"
 done
