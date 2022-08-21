98f6113e056dd7fcd493d64d0586b86e888f3e9c
Ben Thompson <t.ben.thompson@gmail.com>
2022-08-21 22:05:10 +0000

    Successfully pulling docker images on EC2.

--- cloud/connect.sh
+++ cloud/connect.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-URL=$(aws ec2 describe-instances | jq -r ".Reservations[0].Instances[0].PublicDnsName")
+# Connects to the terraform 
+
+URL=$(terraform output -raw ec2instance)
+# alternative command: aws ec2 describe-instances | jq -r ".Reservations[0].Instances[0].PublicDnsName"
 USER="ec2-user"
 
 echo -e "\n Adding AWS key to ssh-agent. \n"
@@ -8,4 +11,4 @@ eval `ssh-agent -s`
 ssh-add - <<< "${AWS_KEY_AWS_KEY_PAIR}"
 
 echo -e "\n Connecting to: $USER@$URL \n"
-ssh "$USER@$URL"
+ssh "$USER@$URL" 
--- cloud/init.sh
+++ cloud/init.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 sudo yum update -y
-sudo yum install -y docker
+sudo yum install -y docker jq
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 
-ACCOUNT=$(aws sts get-caller-identity | jq -r .UserId)
+# https://stackoverflow.com/questions/51597492/how-to-get-aws-account-number-id-based-on-ec2-instance-which-is-hosted-in-amazo
+ACCOUNT=$(aws sts get-caller-identity | jq -r .Account)
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ACCOUNT.dkr.ecr.us-east-1.amazonaws.com
 docker pull $ACCOUNT.dkr.ecr.us-east-1.amazonaws.com/${image_name}
