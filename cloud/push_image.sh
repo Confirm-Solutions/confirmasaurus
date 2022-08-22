2834f60975070249fb95a9187193bab8a7c137b5
Ben Thompson <t.ben.thompson@gmail.com>
2022-08-22 01:20:42 -0400

    pushing image to ghcr.

--- cloud/push_image.sh
+++ cloud/push_image.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-ACCOUNT=$(aws sts get-caller-identity | jq -r .UserId)
-aws ecr get-login-password --region us-east-1 | \
-    docker login --username AWS --password-stdin "$ACCOUNT".dkr.ecr.us-east-1.amazonaws.com
-docker tag smalldev:latest "$ACCOUNT".dkr.ecr.us-east-1.amazonaws.com/smalldev:latest
-docker push "$ACCOUNT".dkr.ecr.us-east-1.amazonaws.com/smalldev:latest
+
+# AWS ECR instructions:
+
+# ACCOUNT=$(aws sts get-caller-identity | jq -r .UserId)
+# aws ecr get-login-password --region us-east-1 | \
+#     docker login --username AWS --password-stdin "$ACCOUNT".dkr.ecr.us-east-1.amazonaws.com
+# docker tag smalldev:latest "$ACCOUNT".dkr.ecr.us-east-1.amazonaws.com/smalldev:latest
+# docker push "$ACCOUNT".dkr.ecr.us-east-1.amazonaws.com/smalldev:latest
+
+# Github ghcr instructions:
+
+echo $GITHUB_TOKEN | docker login ghcr.io -u tbenthompson --password-stdin
+docker tag smalldev ghcr.io/Confirm-Solutions/smalldev:latest
+docker push ghcr.io/confirm-solutions/smalldev:latest
