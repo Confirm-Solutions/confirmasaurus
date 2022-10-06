Fixing bugs in the cv_idx calculation and adagrid refinement criterion.

confirm/confirm/lewislib/batch.py
@@ -69,7 +69,10 @@ def batch(f, batch_size: int, in_axes):
         # batch_size_new = min(batch_size, dim)
         n_full_batches = dim // batch_size
         remainder = dim % batch_size
-        n_pad = batch_size - remainder
+        if remainder == 0:
+            n_pad = 0
+        else:
+            n_pad = batch_size - remainder
         pad_last = remainder > 0
         start = 0
         end = batch_size
@@ -140,7 +143,7 @@ def batch_all_concat(f, batch_size: int, in_axes, out_axes=None):
         return [
             np.concatenate(
                 [
-                    outs[j][i][:-n_pad] if j == last_j else outs[j][i]
+                    outs[j][i][:-n_pad] if (j == last_j and n_pad != 0) else outs[j][i]
                     for j in range(len(outs))
                 ],
                 axis=internal_out_axes[i],

confirm/confirm/mini_imprint/binomial.py
@@ -206,10 +206,14 @@ def zero_order_bound(typeI_sum, sim_sizes, delta, delta_prop_0to1):
     d0 = typeI_sum / sim_sizes
     # clopper-pearson upper bound in beta form.
     d0u_factor = 1.0 - delta * delta_prop_0to1
+    # NOTE: moving this to JAX is nontrivial and probably best done with
+    # tensorflow_probability:
+    # https://github.com/google/jax/issues/2399#issuecomment-1225990206
+    # https://github.com/pyro-ppl/numpyro/blob/e28a3feaa4f95d76b361101f0c75dcb5add2365e/numpyro/distributions/util.py#L426
     d0u = scipy.stats.beta.ppf(d0u_factor, typeI_sum + 1, sim_sizes - typeI_sum) - d0
     # If typeI_sum == sim_sizes, scipy.stats outputs nan. Output 0 instead
     # because there is no way to go higher than 1.0
-    d0u = np.where(np.isnan(d0u), 0, d0u)
+    d0u = jnp.where(jnp.isnan(d0u), 0, d0u)
     return d0, d0u
 
 

confirm/tests/test_holder_odi.py
@@ -73,8 +73,8 @@ def test_holder_odi(d):
 
     np.random.seed(17)
     uniforms = np.random.uniform(size=(nsims, n_arm_samples, d))
-    typeI_sum, _ = binomial.binomial_accumulator(lambda x: x[..., 0] >= thresh)(
-        theta_tiles, is_null_per_arm, uniforms
+    typeI_sum = binomial.binomial_accumulator(lambda x, thresh: x[..., 0] >= thresh)(
+        thresh, theta_tiles, is_null_per_arm, uniforms
     )
 
     typeI_est, typeI_CI = binomial.zero_order_bound(typeI_sum, nsims, delta, 1.0)
@@ -84,9 +84,9 @@ def test_holder_odi(d):
         typeI_bound, theta_tiles, tile_corners, n_arm_samples, holderq
     )
     correct = {1: 0.04163348, 2: 0.37039003}
-    np.testing.assert_allclose(hob[0], correct[d], rtol=1e-6)
+    np.testing.assert_allclose(hob[0], correct[d], atol=2e-6)
 
     pointwise_bound = binomial.invert_bound(
-        theta_tiles, tile_corners, hob, n_arm_samples, holderq
+        hob, theta_tiles, tile_corners, n_arm_samples, holderq
     )
-    np.testing.assert_allclose(pointwise_bound, typeI_bound)
+    np.testing.assert_allclose(pointwise_bound, typeI_bound, atol=1e-7)