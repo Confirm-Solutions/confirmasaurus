d2198bb18b2c66cdab613c061a40e27957697c7f
Ben Thompson <t.ben.thompson@gmail.com>
2022-11-21 11:01:01 -0500

    Batching is unnecessary in ZTest1D.

--- confirm/models/ztest.py
+++ confirm/models/ztest.py
@@ -1,8 +1,6 @@
 import jax
 import jax.numpy as jnp
 
-import confirm.mini_imprint as ip
-
 
 @jax.jit
 def _sim(samples, theta, null_truth):
@@ -15,18 +13,14 @@ def _sim(samples, theta, null_truth):
 
 
 class ZTest1D:
-    def __init__(self, seed, max_K, *, sim_batch_size=2048):
+    def __init__(self, seed, max_K):
         self.family = "normal"
-        self.sim_batch_size = sim_batch_size
         self.dtype = jnp.float32
 
         # sample normals and then compute the CDF to transform into the
         # interval [0, 1]
         key = jax.random.PRNGKey(seed)
         self.samples = jax.random.normal(key, shape=(max_K,), dtype=self.dtype)
-        self._sim_batch = ip.batch(
-            _sim, self.sim_batch_size, in_axes=(0, None, None), out_axes=(1,)
-        )
 
     def sim_batch(self, begin_sim, end_sim, theta, null_truth, detailed=False):
-        return self._sim_batch(self.samples[begin_sim:end_sim], theta, null_truth)
+        return _sim(self.samples[begin_sim:end_sim], theta, null_truth)
