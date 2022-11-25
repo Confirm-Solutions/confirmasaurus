0cc2bbad0033a6947c6fa80b546bd355d277ec4e
James Yang <jamesyang916@gmail.com>
2022-11-24 18:02:29 -1000

    Add edge cases for normal

--- confirm/bound/normal.py
+++ confirm/bound/normal.py
@@ -15,7 +15,11 @@ class ForwardQCPSolver:
     def solve(self, v, f0):
         logf0 = jnp.log(f0)
         mv_sqrt = self.scale * jnp.abs(v)
-        q_opt = jnp.sqrt(-2 * logf0) / mv_sqrt
+        q_opt = jax.lax.cond(
+            mv_sqrt == 0,
+            lambda: jnp.inf,
+            lambda: jnp.sqrt(-2 * logf0) / mv_sqrt,
+        )
         return jnp.maximum(q_opt, 1)
 
 
@@ -31,7 +35,11 @@ class BackwardQCPSolver:
 
     def solve(self, v, alpha):
         mv_sqrt = self.scale * jnp.abs(v)
-        return 1 + jnp.sqrt(-2 * jnp.log(alpha)) / mv_sqrt
+        return jax.lax.cond(
+            mv_sqrt == 0,
+            lambda: jnp.inf,
+            lambda: 1 + jnp.sqrt(-2 * jnp.log(alpha)) / mv_sqrt,
+        )
 
 
 class TileForwardQCPSolver:
@@ -47,7 +55,11 @@ class TileForwardQCPSolver:
     def solve(self, vs, f0):
         logf0 = jnp.log(f0)
         mv_sqrt = self.scale * jnp.max(jnp.abs(vs))
-        q_opt = jnp.sqrt(-2 * logf0) / mv_sqrt
+        q_opt = jax.lax.cond(
+            mv_sqrt == 0,
+            lambda: jnp.inf,
+            lambda: jnp.sqrt(-2 * logf0) / mv_sqrt,
+        )
         return jnp.maximum(q_opt, 1)
 
 
@@ -63,31 +75,61 @@ class TileBackwardQCPSolver:
 
     def solve(self, vs, alpha):
         mv_sqrt = self.scale * jnp.max(jnp.abs(vs))
-        return 1 + jnp.sqrt(-2 * jnp.log(alpha)) / mv_sqrt
+        return jax.lax.cond(
+            mv_sqrt == 0,
+            lambda: jnp.inf,
+            lambda: 1 + jnp.sqrt(-2 * jnp.log(alpha)) / mv_sqrt,
+        )
 
 
 def tilt_bound_fwd(q, scale, v, f0):
     p_inv = 1 - 1 / q
     expo = 0.5 * (q - 1) * (scale * v) ** 2
-    return f0**p_inv * jnp.exp(expo)
+    return jax.lax.cond(
+        (v == 0) | (scale == 0),
+        lambda: f0**p_inv,
+        lambda: jax.lax.cond(
+            q == jnp.inf,
+            lambda: jnp.inf,
+            lambda: f0**p_inv * jnp.exp(expo),
+        ),
+    )
 
 
 def tilt_bound_fwd_tile(q, scale, vs, f0):
     p_inv = 1 - 1 / q
-    max_expo = 0.5 * (q - 1) * (scale * jnp.max(jnp.abs(vs))) ** 2
-    return f0**p_inv * jnp.exp(max_expo)
+    max_v = jnp.max(jnp.abs(vs))
+    max_expo = 0.5 * (q - 1) * (scale * max_v) ** 2
+    return jax.lax.cond(
+        (max_v == 0) | (scale == 0),
+        lambda: f0**p_inv,
+        lambda: jax.lax.cond(
+            q == jnp.inf,
+            lambda: jnp.inf,
+            lambda: f0**p_inv * jnp.exp(max_expo),
+        ),
+    )
 
 
 def tilt_bound_bwd(q, scale, v, alpha):
-    p = 1 / (1 - 1 / q)
-    expo = 0.5 * (q - 1) * (scale * v) ** 2
-    return (alpha * jnp.exp(-expo)) ** p
+    p = jax.lax.cond(q == 1, lambda _: jnp.inf, lambda q: 1 / (1 - 1 / q), q)
+    expo = jax.lax.cond(
+        (v == 0) | (scale == 0),
+        lambda: 0.0,
+        lambda: 0.5 * q * (scale * v) ** 2,
+    )
+    return alpha**p * jnp.exp(-expo)
 
 
 def tilt_bound_bwd_tile(q, scale, vs, alpha):
-    p = 1 / (1 - 1 / q)
-    max_expo = 0.5 * (q - 1) * (scale * jnp.max(jnp.abs(vs))) ** 2
-    return (alpha * jnp.exp(-max_expo)) ** p
+    p = jax.lax.cond(q == 1, lambda _: jnp.inf, lambda q: 1 / (1 - 1 / q), q)
+    max_v = jnp.max(jnp.abs(vs))
+    max_expo = jax.lax.cond(
+        (max_v == 0) | (scale == 0),
+        lambda: 0.0,
+        lambda: 0.5 * q * (scale * max_v) ** 2,
+    )
+    return alpha**p * jnp.exp(-max_expo)
 
 
 class NormalBound:
