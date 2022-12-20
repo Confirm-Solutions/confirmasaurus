943c7f8d58976281628f66f47568e9134095cc67
Ben Thompson <t.ben.thompson@gmail.com>
2022-12-20 16:33:15 +0000

    Mark tutorial tests as slow.

--- imprint/tests/test_tutorials.py
+++ imprint/tests/test_tutorials.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import pandas as pd
+import pytest
 
 from imprint.nb_util import run_notebook
 
@@ -9,12 +10,14 @@ def get_tutorial_path(filename):
     return Path(__file__).resolve().parent.parent.joinpath("tutorials", filename)
 
 
+@pytest.mark.slow
 def test_ztest_tutorial(snapshot):
     ns, _ = run_notebook(get_tutorial_path("ztest.ipynb"))
     rej_df = ns["rej_df"]
     pd.testing.assert_frame_equal(rej_df, snapshot(rej_df))
 
 
+@pytest.mark.slow
 def test_fisher_exact_tutorial(snapshot):
     ns, _ = run_notebook(
         get_tutorial_path("fisher_exact.ipynb"), cell_indices=[0, 3, 4, 7]
@@ -23,6 +26,7 @@ def test_fisher_exact_tutorial(snapshot):
     assert lamss == snapshot(lamss)
 
 
+@pytest.mark.slow
 def test_basket_tutorial(snapshot):
     ns, _ = run_notebook(get_tutorial_path("basket.ipynb"), cell_indices=[0, 1])
     pd.testing.assert_frame_equal(ns["validation_df"], snapshot(ns["validation_df"]))
