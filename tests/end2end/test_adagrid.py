b012b98a4b4ebb3754be0a602d564b237115c7ed
Ben Thompson <t.ben.thompson@gmail.com>
2023-01-31 10:27:01 -0500

    GPU jobs have slightly different floating point outputs.

--- tests/end2end/test_adagrid.py
+++ tests/end2end/test_adagrid.py
@@ -19,7 +19,7 @@ def test_bootstrap_calibrate(snapshot):
     cal_df = ada.bootstrap.bootstrap_calibrate(ZTest1D, g=g, nB=5)
     twb_cols = [c for c in cal_df.columns if "twb_lams" in c]
     np.testing.assert_allclose(cal_df["twb_mean_lams"], cal_df[twb_cols].mean(axis=1))
-    np.testing.assert_allclose(cal_df, snapshot(cal_df))
+    np.testing.assert_allclose(cal_df, snapshot(cal_df), rtol=1e-6)
 
 
 def check(db, snapshot, only_lams=False):
