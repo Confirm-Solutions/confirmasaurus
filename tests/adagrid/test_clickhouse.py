83f69ccbde6ae1f61ae38cda3a446e0a26cb42cb
Ben Thompson <t.ben.thompson@gmail.com>
2023-03-16 09:14:51 -0400

    Fix test failures.

--- tests/adagrid/test_clickhouse.py
+++ tests/adagrid/test_clickhouse.py
@@ -1,41 +1,13 @@
 import pandas as pd
 import pytest
 
-from ..test_db import DBTester
-
-
-class ClickhouseCleanup:
-    def setup_method(self, method):
-        self.dbs = []
-
-    def teardown_method(self, method):
-        import confirm.cloud.clickhouse as ch
-
-        client = ch.get_ch_client()
-        job_ids = [db.job_id for db in self.dbs]
-        for db in self.dbs:
-            db.close()
-        ch.clear_dbs(client, names=job_ids, yes=True)
-
-    def _connect(self):
-        import confirm.cloud.clickhouse as ch
-
-        self.dbs.append(ch.Clickhouse.connect())
-        return self.dbs[-1]
-
-
-@pytest.mark.slow
-class TestClickhouse(DBTester, ClickhouseCleanup):
-    def connect(self):
-        return self._connect()
-
 
 @pytest.mark.slow
 def test_connect_prod_no_job_id():
     import confirm.cloud.clickhouse as ch
 
     with pytest.raises(RuntimeError) as excinfo:
-        ch.Clickhouse.connect(host="fakeprod")
+        ch.connect(host="fakeprod")
     assert "To run a production job" in str(excinfo.value)
 
 
